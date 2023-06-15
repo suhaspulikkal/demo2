a997911e291883fcde36c8174765f6b534498764
Suhas <suhasps05@gmail.com>
2023-06-15 10:31:34 +0530

    Made some changes to solve image not loading issues

--- demo2/src/components/Home.jsx
+++ demo2/src/components/Home.jsx
@@ -32,13 +32,13 @@ function Home() {
       <SkillsBox 
       heading = "Frontend Framework"
       title1 = "React"
-      url1 = "demo2/images/react.png"
+      url1 = "build/images/react.png"
       // url1 = {PF +"/images/react.png"}
       title2 = "JQuery"
-      url2 = "demo2/images/jquery.jpg"
+      url2 = "build/images/jquery.jpg"
       // url2 = {PF +"/images/jquery.jpg"}
       title3 = "Bootstrap"
-      url3 = "demo2/images/bootstrap.png"
+      url3 = "build/images/bootstrap.png"
       // url3 = {PF +"/images/bootstrap.png"}
       />
 
