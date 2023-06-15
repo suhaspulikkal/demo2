2acaa65d09194e508d6c19c23685442e4a36cb19
Suhas <suhasps05@gmail.com>
2023-06-15 09:09:08 +0530

    Updated Deploy codes / Images not loading due to homepage update in settings

--- demo2/src/App.js
+++ demo2/src/App.js
@@ -1,16 +1,14 @@
 import Home from "./components/Home";
-import { Routes, Route} from "react-router-dom"
-import Profile from "./components/Profile";
-import About from "./components/About"
 
 function App() {
   return (
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/profile' element={<Profile />}></Route>
-          <Route path='/about' element={<About />}></Route>
+    <Home />
+        // <Routes>
+        //   <Route path='/' element={<Home />}></Route>
+        //   <Route path='/profile' element={<Profile />}></Route>
+        //   <Route path='/about' element={<About />}></Route>
 
-        </Routes>
+        // </Routes>
       
     );
 }
