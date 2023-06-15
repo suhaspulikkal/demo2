825fb8577e959b32c7b104629eb478ad3c995d5e
Suhas <suhasps05@gmail.com>
2023-06-15 09:32:13 +0530

    Images/Css not working issue rectified

--- demo2/src/components/Home.jsx
+++ demo2/src/components/Home.jsx
@@ -5,7 +5,7 @@ import SkillsBox from "./SkillsBox";
 import Footer from "./Footer";
 
 function Home() {
-  // const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
   return (
     <div>
@@ -18,28 +18,28 @@ function Home() {
       title1 = "Html" 
       title2 = "Css" 
       title3 = "JS"
-      url1 = "/images/html.png"
-      url2 = "/images/css.png"
-      url3 = "/images/js.png"
-      // url1 = {PF +"/images/html.png"}
-      // url2 = {PF +"/images/css.png"}
-      // url3 = {PF +"/images/js.png"}
+      // url1 = "/images/html.png"
+      // url2 = "/images/css.png"
+      // url3 = "/images/js.png"
+      url1 = {PF +"/images/html.png"}
+      url2 = {PF +"/images/css.png"}
+      url3 = {PF +"/images/js.png"}
       />
       
-      <Banner url ="/images/banner.jpg"/>
-      {/* <Banner url = {PF +"/images/banner.jpg"} /> */}
+      {/* <Banner url ="/images/banner.jpg"/> */}
+      <Banner url = {PF +"/images/banner.jpg"} />
 
       <SkillsBox 
       heading = "Frontend Framework"
       title1 = "React"
-      url1 = "/images/react.png"
-      // url1 = {PF +"/images/react.png"}
+      // url1 = "/images/react.png"
+      url1 = {PF +"/images/react.png"}
       title2 = "JQuery"
-      url2 = "/images/jquery.jpg"
-      // url2 = {PF +"/images/jquery.jpg"}
+      // url2 = "/images/jquery.jpg"
+      url2 = {PF +"/images/jquery.jpg"}
       title3 = "Bootstrap"
-      url3 = "/images/bootstrap.png"
-      // url3 = {PF +"/images/bootstrap.png"}
+      // url3 = "/images/bootstrap.png"
+      url3 = {PF +"/images/bootstrap.png"}
       />
 
       <Footer />
